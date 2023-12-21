cc155486b5cb33f796b7a30755bcee39f0bfabec
iamraselmolla <raselmolla6336@gmail.com>
2023-12-21 16:20:00 +0600

    added google and facebook icons

--- src/Componenets/Header/page/home/Banner/Banner.js
+++ src/Componenets/Header/page/home/Banner/Banner.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './banner.css'
+import { Button } from 'react-bootstrap';
 
 const Banner = () => {
     return (
         <section className='banner' style={{ backgroundImage: `url('img/homeBanner.png')` }}>
+            <div className="container">
+                <Button className='bg-transparent position-relative d-block d-sm-block d-md-none text-white float-end  border border-white '>
+                    Join Group
+                </Button>
+            </div>
             <div className="container position-relative text-white">
+
                 <div className='banner-header' style={{ marginTop: '20%' }}>
                     <h1 className=''>
                         Computer Engineering
--- src/Componenets/Header/page/home/Home.js
+++ src/Componenets/Header/page/home/Home.js
@@ -22,13 +22,26 @@ const Home = () => {
                             <a className="text-decoration-none px-2 fw-bold text-black" href="/">Jobs</a>
                             <a className="text-decoration-none px-2 fw-bold text-black" href="/">Education</a>
                         </div>
-                        <div className="post-part">
-                            <button className="px-3 fw-bolder py-2 rounded border-0" style={{ background: 'rgb(237, 238, 240)' }}>Write a Post <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path fill="none" d="M0 0h24v24H0z"></path><path d="M7 10l5 5 5-5z"></path></svg>
-                            </button>
 
-                            <button className="px-3 bg-primary text-white fw-bolder py-2 rounded border-0 ms-4">
-                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 640 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"></path></svg> Join group </button>
+                        <div className="post-part">
+                            <div className=' d-none d-sm-none d-md-block'>
+                                <button className="px-3 fw-bolder py-2 rounded border-0" style={{ background: 'rgb(237, 238, 240)' }}>Write a Post <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill="none" d="M0 0h24v24H0z"></path><path d="M7 10l5 5 5-5z"></path></svg>
+                                </button>
+
+                                <button className="px-3 bg-primary text-white fw-bolder py-2 rounded border-0 ms-4">
+                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 640 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"></path></svg> Join group </button>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="d-block d-sm-block d-md-none d-flex justify-content-between align-items-center ">
+
+                        <div>
+                            <div className="text-decoration-none px-2 fw-bold text-black text-light ">All Posts (32)</div>
+                        </div>
+                        <div>
+                            <select className="me-3 bg-light  fw-bolder border-0 px-2 py-2 fw-bold" name="" id=""><option value="">Filter: All</option><option value="">Article</option><option value="">Education</option><option value="">MeetUp</option><option value="">Jobs</option></select>
                         </div>
                     </div>
                 </div>
